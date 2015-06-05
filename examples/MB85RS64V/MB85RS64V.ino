b6492bfc2a0a7b5c3332c15edbcaf6cf2689d361
Bruce Pomeroy <bruce@brucepomeroy.com>
2015-06-05 11:03:29 -0700

    Updated example for Mega using hardware SPI and made examples more relevant for our use

--- examples/MB85RS64V/MB85RS64V.ino
+++ examples/MB85RS64V/MB85RS64V.ino
@@ -2,15 +2,16 @@
 #include "Adafruit_FRAM_SPI.h"
 
 /* Example code for the Adafruit SPI FRAM breakout */
-uint8_t FRAM_CS = 10;
+uint8_t FRAM_CS = 53; // Should be 10 for Uno, 53 for Mega
+Adafruit_FRAM_SPI fram = Adafruit_FRAM_SPI(FRAM_CS);  // use hardware SPI
 
-//Adafruit_FRAM_SPI fram = Adafruit_FRAM_SPI(FRAM_CS);  // use hardware SPI
-
+/*
+// Or use software SPI, any pins!
 uint8_t FRAM_SCK= 13;
 uint8_t FRAM_MISO = 12;
 uint8_t FRAM_MOSI = 11;
-//Or use software SPI, any pins!
 Adafruit_FRAM_SPI fram = Adafruit_FRAM_SPI(FRAM_SCK, FRAM_MISO, FRAM_MOSI, FRAM_CS);
+*/
 
 uint16_t          addr = 0;
 
@@ -24,25 +25,33 @@ void setup(void) {
     while (1);
   }
   
+  // Check the datasheet. If it says something like: The 1-Mbit serial F-RAM requires a 3-byte address then you need to specify 3, otherwise pass 2 (the default) or omit this line:
+  fram.setAddressLengthInBytes(3);
+  
   // Read the first byte
   uint8_t test = fram.read8(0x0);
-  Serial.print("Restarted "); Serial.print(test); Serial.println(" times");
+  Serial.print("Incremented first byte "); Serial.print(test); Serial.println(" times");
 
   // Test write ++
   fram.writeEnable(true);
-  fram.write8(0x0, test+1);
+  Serial.print("Incrementing first byte ")
+  fram.write8(0x0, test + 1);
   fram.writeEnable(false);
-
+  
   fram.writeEnable(true);
-  fram.write(0x1, (uint8_t *)"FTW!", 5);
+  Serial.println("Writing test sequence");
+  uint8_t sequenceToWrite[4] = { 0x22, 0x33, 0x44, 0x55 };
+  int addressToStartWritingSequenceAt = 0x1;
+  int lengthOfSequence = 4;
+  fram.write(addressToStartWritingSequenceAt, sequenceToWrite, lengthOfSequence);
   fram.writeEnable(false);
 
-  // dump the entire 8K of memory!
+  // dump first 100 bytes of memory
   uint8_t value;
-  for (uint16_t a = 0; a < 8192; a++) {
+  for (uint16_t a = 0; a < 100; a++) {
     value = fram.read8(a);
     if ((a % 32) == 0) {
-      Serial.print("\n 0x"); Serial.print(a, HEX); Serial.print(": ");
+      Serial.print("\n");
     }
     Serial.print("0x"); 
     if (value < 0x1) 
